fadffa253e21e4bb0aa0f81f03a0a1fcd7c07af7
JuanSesar <juanmsesar@gmail.com>
2023-09-28 13:42:48 -0300

    ajustes en items styles2

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,29 +1,29 @@
 
 import React from 'react';
 import {CartWidget} from "../CartWidget"
-
+import style from "./style.module.css"
 import { Link } from 'react-router-dom';
 export const NavBar = () => {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-  <Link to="/category/home">BIRRERIA</Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+  <Link className={style['birreria']} to="/category/home">BIRRERIA</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-        <Link to="/category/home">Home</Link>
-        </li>
-        <li class="nav-item">
-          <Link   to="category/oferta">Ofertas</Link>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+        <Link className={style['links']} to="/category/home">Home</Link>
         </li>
         <li className="nav-item">
-        <Link  to="category/novedades">Novedades</Link>
+          <Link className={style['links']}  to="category/oferta">Ofertas</Link>
         </li>
-        <li class="nav-item">
-          <a href="">
+        <li className="nav-item">
+        <Link className={style['links']} to="category/novedades">Novedades</Link>
+        </li>
+        <li className="nav-item">
+          <a className={style['links']} href="">
           <CartWidget/>
           </a>
         </li>
