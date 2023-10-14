04b1a020539b5cc14486866fc558caa59d85ff96
JuanSesar <juanmsesar@gmail.com>
2023-10-14 16:39:52 -0300

    solucion clearCart

--- src/components/Cart/cart.jsx
+++ src/components/Cart/cart.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import {  CartProvider} from "../../context/CartProvider"
 import style from "./style.module.css"
 const Cart = () => {
-    const { cart, totalPrice, vaciarCarrito } = useContext(CartProvider)
+    const { cart, totalPrice, clearCart } = useContext(CartProvider)
 
     if (cart.length === 0) {
         return (
@@ -28,7 +28,7 @@ const Cart = () => {
             </div>
             <div className={style['container']} >
                 <Link className={style['links']} to= '/checkout' >Ir a pagar</Link>
-                <button className={style['links']} onClick={() => vaciarCarrito()} >Limpiar carrito</button>
+                <button className={style['links']} onClick={() => clearCart()} >Limpiar carrito</button>
             </div>
         </div>
     )
